8ffbbd4d083427b39d5f67d2aa205a03de29dbe2
kush parsaniya <kushparsaniya94@gmail.com>
2024-10-20 09:40:13 +0530

    Add config-server in discovery service and add role based security in api-gateway.

--- api-gateway/src/main/java/dev/kush/apigateway/config/WebSecurityConfig.java
+++ api-gateway/src/main/java/dev/kush/apigateway/config/WebSecurityConfig.java
@@ -2,15 +2,19 @@ package dev.kush.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class WebSecurityConfig {
@@ -24,14 +28,60 @@ public class WebSecurityConfig {
                 .authorizeExchange(
                         exchange -> exchange
                                 .pathMatchers(freeResourceUrls).permitAll()
+                                .pathMatchers(HttpMethod.GET, "/products/**").hasAnyAuthority("ADMIN", "USER")
+                                .pathMatchers(HttpMethod.POST, "/products/**").hasAuthority("ADMIN")
                                 .anyExchange().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(configurationSource()))
-                .oauth2ResourceServer(rs -> rs.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(rs -> rs.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
         return http.build();
     }
 
+    // blocking
+    //    @Bean
+//    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+//        ReactiveJwtAuthenticationConverter converter = new ReactiveJwtAuthenticationConverter();
+//        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+//            var authorities = new HashSet<GrantedAuthority>();
+//
+//            // Extract roles from resource_access for the specific client
+//            var resourceAccess = jwt.getClaimAsMap("resource_access");
+//            if (resourceAccess != null && resourceAccess.containsKey("microservice01")) {
+//                var clientRoles = (Map<String, Object>) resourceAccess.get("microservice01");
+//                if (clientRoles.containsKey("roles")) {
+//                    var roles = (List<String>) clientRoles.get("roles");
+//                    roles.forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
+//                }
+//            }
+//            return Flux.fromIterable(authorities).collectList().flatMapMany(Flux::fromIterable);
+//        });
+//        return converter;
+//    }
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        ReactiveJwtAuthenticationConverter converter = new ReactiveJwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            return Mono.justOrEmpty(jwt.getClaimAsMap("resource_access"))
+                    .flatMapMany(resourceAccess -> {
+                        if (resourceAccess != null && resourceAccess.containsKey("microservice01")) {
+                            var clientRoles = (Map<String, Object>) resourceAccess.get("microservice01");
+                            if (clientRoles.containsKey("roles")) {
+                                var roles = (List<String>) clientRoles.get("roles");
+                                return Flux.fromIterable(roles)
+                                        .map(role -> new SimpleGrantedAuthority(role))
+                                        .collectList()
+                                        .flatMapMany(Flux::fromIterable);
+                            }
+                        }
+                        return Flux.empty();
+                    });
+        });
+        return converter;
+    }
+
+
+    @Bean
     CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
